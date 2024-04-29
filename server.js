8de92b13e50b180f0e276ccbc3009e9ffe6ce0b4
Chance Strickland <hi@chance.dev>
2024-04-29 10:17:44 -0700

    format

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ import morgan from "morgan";
 installGlobals();
 
 const viteDevServer =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : await import("vite").then((vite) =>
-        vite.createServer({
-          server: { middlewareMode: true },
-        })
-      );
+	process.env.NODE_ENV === "production"
+		? undefined
+		: await import("vite").then((vite) =>
+				vite.createServer({
+					server: { middlewareMode: true },
+				})
+		  );
 
 const remixHandler = createRequestHandler({
-  build: viteDevServer
-    ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
-    : await import("./build/server/index.js"),
+	build: viteDevServer
+		? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+		: await import("./build/server/index.js"),
 });
 
 const app = express();
@@ -30,13 +30,13 @@ app.disable("x-powered-by");
 
 // handle asset requests
 if (viteDevServer) {
-  app.use(viteDevServer.middlewares);
+	app.use(viteDevServer.middlewares);
 } else {
-  // Vite fingerprints its assets so we can cache forever.
-  app.use(
-    "/assets",
-    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
-  );
+	// Vite fingerprints its assets so we can cache forever.
+	app.use(
+		"/assets",
+		express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+	);
 }
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
@@ -50,5 +50,5 @@ app.all("*", remixHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
-  console.log(`Express server listening at http://localhost:${port}`)
+	console.log(`Express server listening at http://localhost:${port}`)
 );
